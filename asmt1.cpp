ea8171d32352fe6d6100aea773e71a8a72da8436
David Browning <dmbrowning21@gmail.com>
2016-08-31 10:37:28 -0600

    aster
    e working, set to 30 precision

--- asmt1.cpp
+++ asmt1.cpp
@@ -1,5 +1,6 @@
 #include<iostream> 
 #include<string>
+#include<iomanip>
 
 double computePi(int x){
 /*
@@ -8,11 +9,47 @@ double computePi(int x){
 return 0.0;
 }
 
+double factorial(int i){
+	int j = i;
+	std::cout << i << " factorial is: ";
+	while(j > 1){
+		i *= (j-1);
+		--j;
+	}
+	std::cout << i << std::endl << std::endl;
+
+	return i;
+}
+
 double computeE(int x){
 /*
-
+handle cases up to three and less than 0
+Then, starting at 2, add each iteration to n (which initially stores 2) until you've reached x
+create factorial(int i) function. 
+return e.
 */
-return 0.0;
+double e = 0;
+	if(x < 3){
+		if (x <= 0){
+			std::cout << "undefined" << std::endl;
+		}
+		else if(x == 1){
+			e = 1;
+		}
+		else if (x == 2){
+			e = 2;
+		}
+	}	
+	else{
+		e = 2;
+		for(int i = 2; i <= x; ++i){
+			e += (1/factorial(i));
+		}
+	
+	}
+
+std::cout  << (1/.7) << std::endl;
+return e;
 }
 
 int computeFibonacci(int x){
@@ -63,7 +100,9 @@ int main(int argc, char* argv[])
 		switch(optionNumber){
 			case(1): computeFibonacci(num);
 			 break;
-			case(2): computeE(num);
+			case(2): {double ans = computeE(num);
+				std::cout << "e = " << std::setprecision(30) <<  ans << std::endl;
+			}
 			 break;
 			case(3): computePi(num);
 			 break;
